8852bc25fc1755e33016960c13c52f4de9b31b09
parman <vharman.irwan@gmail.com>
2014-03-21 11:31:47 +0700

    versi 2.3.2

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+echo '$arr = array("'.implode('", "', $arraydata).'");';
+echo'
+Array (
+	[0] => 
+		Array ( 
+				[pro_id] => IDBA0000015  Fast Boat ke Gili Trawangan xxxx
+				[price_id] => Array ( [0] => 111 ) 
+				[qty] => Array ( [0] => 1 ) 
+				[day] => 1 
+		) 
+	[1] => 
+		Array (  
+				[pro_id] => IDBA0000052 Wisata Kapal Selam	xxxxxx
+				[price_id] => Array ( [0] => 166 ) 
+				[qty] => Array ( [0] => 1 ) 
+				[day] => 1 
+		) 
+	[2] => 
+		Array ( 
+				[pro_id] => IDBA0000007 Dinner Cruise Romantis xxxxx
+				[price_id] => Array ( [0] => 77 [1] => 78 ) 
+				[qty] => Array ( [0] => 1 [1] => 2 ) 
+				[day] => 1 
+		) 
+	[3] => 
+		Array ( 
+				[pro_id] => IDBA0000048  Warung Babi Guling Ibu Oka xxxx
+				[price_id] => Array ( [0] => 110 ) 
+				[qty] => Array ( [0] => 1 ) 
+				[day] => 2 ) 
+	[4] => 
+		Array ( 
+				[pro_id] => IDBA0000020 Menunggang Gajah xxxx
+				[price_id] => Array ( [0] => 73 [1] => 74 ) 
+				[qty] => Array ( [0] => 1 [1] => 1 ) 
+				[day] => 2 ) 
+	[5] => 
+		Array ( 
+				[pro_id] => IDBA0000049  The Pond Restaurant xxxx
+				[price_id] => Array ( [0] => 126 ) 
+				[qty] => Array ( [0] => 1 ) 
+				[day] => 3 ) 
+	[6] => 
+		Array ( 
+				[pro_id] => IDBA0000029 Pura Tirta Empul
+				[price_id] => Array ( [0] => 254 [1] => 256 ) 
+				[qty] => Array ( [0] => 1 [1] => 1 ) 
+				[day] => 3 ) 
+	[7] => 
+		Array ( 
+				[pro_id] => IDBA0000052 Wisata Kapal Selam xxx
+				[price_id] => Array ( [0] => 166 ) 
+				[qty] => Array ( [0] => 1 ) 
+				[day] => 4 ) )';
+?>
